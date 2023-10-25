fb220e02532d8672e0b5550717ea44905953ed2c
NiteshKumar133 <135575205+NiteshKumar133@users.noreply.github.com>
2023-10-25 19:44:24 +0530

    Update about.jsx

--- src/content/about.jsx
+++ src/content/about.jsx
@@ -5,10 +5,9 @@ export default function About() {
     <div className='about-parent-container'>
         <h1 style={{textAlign:"center"}}>About me</h1>
         <div className='about-child-container'>
-       <ul>
-       <li><span className='triangular-bullet'>‣</span>I am a BCA Student at <a href="https://www.cimage.in/" target='__blank' style={{textDecoration:"none"}} className='cimage-gdg'>CIMAGE</a> College, Patna.</li>
-       <li><span className='triangular-bullet'>‣</span>I am volunteer at <a href="https://www.gdgpatna.com/"  target='__blank' style={{textDecoration:"none"}} className='cimage-gdg'>GDG</a> Patna.</li>
-       </ul>
+      <h3 className='about-child-container-h4'> <span className='triangular-bullet'>‣</span>I am a BCA Student at <a href="https://www.cimage.in/" target='__blank' style={{textDecoration:"none"}} className='cimage-gdg'>CIMAGE</a> College, Patna.</h3>
+       <h3 className='about-child-container-h4'><span className='triangular-bullet'>‣</span>I am volunteer at <a href="https://www.gdgpatna.com/"  target='__blank' style={{textDecoration:"none"}} className='cimage-gdg'>GDG</a> Patna.</h3>
+      
        </div>  
     </div>
   )
