8647a5340faa1f980a00544d8bced7ff3c660fc0
NiteshKumar133 <135575205+NiteshKumar133@users.noreply.github.com>
2023-11-19 06:15:00 +0530

    Update header.jsx

--- src/content/header.jsx
+++ src/content/header.jsx
@@ -1,6 +1,5 @@
 import React,{useEffect} from 'react';
 import './header.css';
-import pic from './profile/WhatsApp_Image_2023-10-23_at_4.06.05_PM-removebg-preview.png';
 import TextTransition, { presets } from 'react-text-transition';
 export function Showprofile() {
   const TEXTS = ['Web Developer', 'Game Developer'];
@@ -20,12 +19,12 @@ export function Showprofile() {
        <div className='show-about-name'>
         <div className='header-line'>
         <h1 className='hello-i-am'>Hello,</h1>
-       <h1 className='hello-i-am'>I am Nitesh kumar</h1>
+       <h1 className='hello-i-am'>I am Name...</h1>
         </div>
        
        <h2 className='I-am-Full-stack'>I am a <span className='element' style={{color:"blue"}}><TextTransition springConfig={presets.wobbly} inline="true">{TEXTS[index % TEXTS.length]}</TextTransition></span></h2>
        </div>
-<img src={pic} className='profile-pic' alt="profile-pic"/>
+<img src={'https://images.app.goo.gl/DpjEVuPQuhMM2mDj7'} className='profile-pic' alt="profile-pic"/>
 
     </div>
   )
